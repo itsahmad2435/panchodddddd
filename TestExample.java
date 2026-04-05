0c51aa96b3672fbb5cc77cd742beddecef5f8349
Ahmad Nisar <ahmadnanisar3582@gmail.com>
2026-04-05 15:05:49 +0500

    JUnit project

---
+++ TestExample.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestExample {
+    @Test
+    void testPositive() {
+        assertTrue(5 > 0);
+    }
+}
